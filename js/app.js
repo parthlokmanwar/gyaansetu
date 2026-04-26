35e80cfacd4e18393515461dffcfb8bded41fb3d
dhruv doshi <dhruvdoshi.pvt@gmail.com>
2026-04-26 21:38:00 +0530

    fix: Update YouTube regex to support shorts and extra params

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ function getInitials(name) {
 }
 
 function extractYouTubeId(url) {
-  const regex = /(?:youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  const regex = /(?:youtube\.com\/(?:watch\?(?:.*&)?v=|embed\/|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
   const match = url.match(regex);
   return match ? match[1] : null;
 }
